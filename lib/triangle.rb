4c6bc36d2f5f90f78c5f19594aea7a8d000ce83b
Berry  Semexan <bsemexan@gmail.com>
2018-02-21 20:02:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,14 @@
 class Triangle
   # triangle code
-  attr_accessor :a, :b, :c 
+  attr_accessor :a, :b, :c
 
   def initialize(a, b, c)
-    @a = a 
-    @b = b 
+    @a = a
+    @b = b
     @c = c
   end
+
+  def invalid triangle
 end
 
 class TriangleError < StandardError
