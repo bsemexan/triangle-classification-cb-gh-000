92f3b8f57465bc4ee588390ad0252ad27b64f4f4
Berry  Semexan <bsemexan@gmail.com>
2018-02-21 20:11:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
 
   def invalid_triangle
-    if a<=0 || b<=0 || c<=0 || (a+b)<=c || (b+c)<=a || (a+b)<=b
+    if a<=0 || b<=0 || c<=0 || (a+b)<=c || (b+c)<=a || (a+c)<=b
       raise TriangleError
     end
   end
